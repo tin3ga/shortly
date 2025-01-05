cd9ef045a6cedd0d06cea41a0aafb66a56344247
tin3ga <118301331+tin3ga@users.noreply.github.com>
2025-01-05 20:33:32 +0300

    Add SQL queries for retrieving and deleting short links

--- sql/queries/links.sql
+++ sql/queries/links.sql
@@ -3,7 +3,13 @@ INSERT INTO shortly(id, short_link, long_link)
 VALUES($1, $2, $3)
 RETURNING *;
 
+-- name: GetLongLink :one
+SELECT * FROM shortly
+WHERE short_link = $1
+LIMIT 1;
 
+-- name: DeleteLink :exec
+DELETE FROM shortly WHERE short_link = $1;
 
 -- name: ListLinks :many
 SELECT * FROM shortly
