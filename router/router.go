ad77977d202eeb9cc3fa942f487dd579bbe17d02
tin3ga <118301331+tin3ga@users.noreply.github.com>
2025-01-12 21:18:41 +0300

    feat: enhance routing setup to include JWT secret and user, link management

--- router/router.go
+++ router/router.go
@@ -9,24 +9,48 @@ import (
 
 	"github.com/tin3ga/shortly/handler"
 	"github.com/tin3ga/shortly/internal/database"
+	"github.com/tin3ga/shortly/middleware"
 )
 
 // SetupRoutes setup router api
-func SetupRoutes(app *fiber.App, queries *database.Queries, ctx context.Context, rdb *redis.Client, ttl time.Duration, urlStr string, apiKey string) {
+func SetupRoutes(app *fiber.App, queries *database.Queries, ctx context.Context, rdb *redis.Client, ttl time.Duration, urlStr string, apiKey string, jwtsecret string) {
 	app.Get("/", handler.Ping)
-
-	// shortly
-	app.Get("/api/v1/", func(c *fiber.Ctx) error {
-		return handler.GetLinks(c, queries, ctx)
-	})
-	app.Get("/api/v1/:link", func(c *fiber.Ctx) error {
+	app.Get("/:link", func(c *fiber.Ctx) error {
 		return handler.GetLink(c, queries, ctx, rdb, ttl)
 	})
 
-	app.Post("/api/v1/shorten", func(c *fiber.Ctx) error {
+	api := app.Group("api/v1")
+
+	// user
+	user := api.Group("/users")
+	user.Post("/", func(c *fiber.Ctx) error {
+		return handler.CreateUser(c, queries, ctx)
+
+	})
+	user.Delete("/delete", func(c *fiber.Ctx) error {
+		return handler.DeleteUser(c, queries, ctx)
+	})
+
+	// auth
+	auth := api.Group("/auth")
+	auth.Post("/", func(c *fiber.Ctx) error {
+		return handler.Login(c, queries, ctx, jwtsecret)
+	})
+
+	// shortly
+
+	links := api.Group("links")
+	links.Get("/all", func(c *fiber.Ctx) error {
+		return handler.GetLinks(c, queries, ctx)
+	})
+	links.Get("/userlinks", func(c *fiber.Ctx) error {
+		return handler.GetUserLinks(c, queries, ctx)
+	})
+
+	links.Post("/shorten", middleware.Protected(), func(c *fiber.Ctx) error {
 		return handler.ShortenLink(c, queries, ctx, urlStr, apiKey)
 	})
-	app.Delete("/api/v1/shorten", func(c *fiber.Ctx) error {
+	links.Delete("/shorten", middleware.Protected(), func(c *fiber.Ctx) error {
 		return handler.DeleteLink(c, queries, ctx)
 	})
 }
